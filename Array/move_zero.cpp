7a48ab2569913d6be10c301202b4a50f0403fd8d
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2023-07-07 19:01:58 +0530

    question slove

---
+++ Array/move_zero.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[cnt] = nums[i];
+                cnt++;
+            }
+        }
+        while (cnt < nums.size())
+        {
+            nums[cnt] = 0;
+            cnt++;
+        }
+    }
+};
